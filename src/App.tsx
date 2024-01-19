1ddb175da4f16d1085f393f1a56817151e24462f
caiomelotec <caio.melotec@icloud.com>
2024-01-19 23:18:15 +0100

    register

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { Login } from "./components/Login.tsx";
+import { Register } from "./components/Register.tsx";
 
 function App() {
   return (
     <>
-      <Login />
+      <Register />
     </>
   );
 }
