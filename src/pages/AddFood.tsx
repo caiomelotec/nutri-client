bb67b0c566f06bda9d4ab7b5ad22068ea0f41d1f
caiomelotec <caio.melotec@icloud.com>
2024-01-27 20:40:35 +0100

    addfood page

---
+++ src/pages/AddFood.tsx
@@ -0,0 +1,10 @@
+import { AddFoodForm } from "../components/AddFoodForm"
+
+
+export const AddFood = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen dark:bg-gray-800">
+        <AddFoodForm/>
+    </div>
+  )
+}
